6a26422e2cdc3348eab7188f2c2a6d742aabcfa9
root <root@ip-10-42-209-97.ec2.internal>
2025-08-28 15:35:32 +0000

    Prints the alphabet in lowercase, then in uppercase,  * followed by a new line. Uses only putchar three times

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase, then in uppercase,
+ * followed by a new line. Uses only putchar three times.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
