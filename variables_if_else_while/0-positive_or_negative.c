da4b35f5e7b402fe882f9963821860f59f5f1c06
root <root@ip-10-42-209-97.ec2.internal>
2025-08-28 14:26:12 +0000

    Positive anything is better than negative nothing

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -7,6 +7,7 @@
  * return = 0
  * Positive anything is better than negative nothing
  */
+
 int main(void)
 {
   int n;
@@ -15,17 +16,17 @@ int main(void)
   n = rand() - RAND_MAX / 2;
 
   if (n > 0)
-    {
+  {
       printf("%d is positive\n", n);
-    }
+  }
   else if (n == 0)
-    {
+  {
       printf("%d is zero\n", n);
-    }
+  }
   else
-    {
+  {
       printf("%d is negative\n", n);
-    }
+  }
 
   return (0);
 }
