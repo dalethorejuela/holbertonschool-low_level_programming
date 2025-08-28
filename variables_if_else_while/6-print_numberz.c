4c4803e903de4b5c3f6f2e560fa3cd452dbdfde4
root <root@ip-10-42-136-5.ec2.internal>
2025-08-28 18:02:19 +0000

    Prints all single digit numbers of base 10 starting from 0,followed by a new line. No char variables allowed

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line. No char variables allowed.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar(num + '0');
+	putchar('\n');
+
+	return (0);
+}
