f447907ec7ee49bb3bad0912795005e7828136e1
root <root@ip-10-42-149-247.ec2.internal>
2025-09-06 01:17:16 +0000

    Write a function that prints a string, in reverse, followed by a new line

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse.
+ * @s: Pointer to string '\0'.
+ *
+ * Return: N.
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	while (len > 0)
+	{
+		len--;
+		_putchar(s[len]);
+	}
+	_putchar('\n');
+}
